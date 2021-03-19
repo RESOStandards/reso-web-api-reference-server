971a40cfedd7e68bbd1ba1bc0bfe572cc1df76d7
michaelpede <mpede@t4bi.com>
2021-03-19 13:24:53 -0700

    needed to make shell scripts executable
    dos2unix is your friend.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM java:8
 
-#COPY /build/reso.jar
+COPY ./temp/spring-server-generated/target/swagger-spring-1.0.0.jar reso.jar
 
 ENTRYPOINT ["java","-jar","reso.jar"]
--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+unzip temp/resources/spring-server-generated.zip -d temp/spring-server-generated
+
+mvn -f temp/spring-server-generated compile
+mvn -f temp/spring-server-generated package
+
 docker-compose build
+rm -rf temp/spring-server-generated
